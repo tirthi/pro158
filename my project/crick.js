d8544a219d204094da845225331501942107b08a
tirthi <91417914+tirthi@users.noreply.github.com>
2023-05-03 06:37:42 -0700

    Add files via upload

---
+++ my project/crick.js
@@ -0,0 +1,98 @@
+AFRAME.registerComponent("tour", {
+    init: function () {
+      this.placesContainer = this.el;   
+      this.createCards();
+    },
+  
+    createCards: function () {
+      const thumbNailsRef = [
+        {
+          id: "narendra modi stadium",
+          title: "Narendra Modi Stadium",
+          url: "./assets/stadium01.jpg",
+        },
+        {
+          id: "Dharamshala Criket Stadium",
+          title: "Dharamshala Criket Stadium",
+          url: "./assets/Dharamshala.jpg",
+        },
+  
+        {
+          id: "wankhede stadium",
+          title: "Wankhede Stadium",
+          url: "./assets/wankhede_stadium.jpg",
+        },
+        {
+          id: "lords",
+          title: "Lords",
+          url: "./assets/lords.jpg",
+        },
+      ];
+      
+      let prevoiusXPosition = -60;
+  
+      for (var item of thumbNailsRef) {
+        const posX = prevoiusXPosition + 25;
+        const posY = 10;
+        const posZ = -40;
+        const position = { x: posX, y: posY, z: posZ };
+        prevoiusXPosition = posX;
+  
+        // Border Element
+        const borderEl = this.createBorder(position, item.id);
+  
+        // Thumbnail Element
+        const thumbNail = this.createThumbNail(item);
+        borderEl.appendChild(thumbNail);
+  
+        // Title Text Element
+        const titleEl = this.createTitleEl(position, item);
+        borderEl.appendChild(titleEl);
+  
+        this.placesContainer.appendChild(borderEl);
+      }
+    },
+    createBorder: function (position, id) {
+      const entityEl = document.createElement("a-entity");
+      entityEl.setAttribute("id", id);
+      entityEl.setAttribute("visible", true);
+      entityEl.setAttribute("geometry", {
+        primitive: "ring",
+        radiusInner: 9,
+        radiusOuter: 10,
+      });
+      entityEl.setAttribute("position", position);
+      entityEl.setAttribute("material", {
+        color: "#0077CC",
+        opacity: 1,
+      });
+  
+      return entityEl;
+    },
+    createThumbNail: function (item) {
+      const entityEl = document.createElement("a-entity");
+      entityEl.setAttribute("visible", true);
+      entityEl.setAttribute("geometry", {
+        primitive: "circle",
+        radius: 9,
+      });
+      entityEl.setAttribute("material", { src: item.url });
+  
+      return entityEl;
+    },
+    createTitleEl: function (position, item) {
+      const entityEl = document.createElement("a-entity");
+      entityEl.setAttribute("text", {
+        font: "exo2bold",
+        align: "center",
+        width: 70,
+        color: "#e65100",
+        value: item.title,
+      });
+      const elPosition = position;
+      elPosition.y = -20;
+      entityEl.setAttribute("position", elPosition);
+      entityEl.setAttribute("visible", true);
+      return entityEl;
+    },
+  });
---
+++ my project/curssor.js
@@ -0,0 +1,49 @@
+AFRAME.registerComponent("cursor-listener", {
+    schema: {
+      selectedItemId: { default: "", type: "string" },
+    },
+    init: function () {
+      this.handleMouseEnterEvents();
+      this.handleMouseLeaveEvents();
+    },
+  
+    handlePlacesListState: function () {
+      const id = this.el.getAttribute("id");
+      const placesId = ["Dharamshala", "lords", "stadium01", "wankhede"];
+      if (placesId.includes(id)) {
+        const placeContainer = document.querySelector("#places-container");
+        placeContainer.setAttribute("cursor-listener", {
+          selectedItemId: id,
+        });
+        this.el.setAttribute("material", {
+          color: "Red",
+          opacity: 2,
+        });
+      }
+    },
+    handleMouseEnterEvents: function () {
+      // Mouse Enter Events
+      this.el.addEventListener("mouseenter", () => {
+        this.handlePlacesListState();
+      });
+    },
+    handleMouseLeaveEvents: function () {
+      // Mouse Leave Events
+      this.el.addEventListener("mouseleave",()=>{
+        const {selectedItemId}=this.data;
+        if(selectedItemId){
+          const el=document.querySelector(`#${selectedItemId}`);
+          const id=el.getAttribute("id");
+          if(id==selectedItemId){
+            el.setAttribute("material",{
+              color:"Blue",
+              opacity:1,
+            })
+          }
+  
+        }
+      })
+      
+    },
+  });
+  
